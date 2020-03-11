5b542060590f6ff494eeb5f4575298e6582511ce
addhire <destiny5313@naver.com>
2020-03-11 11:08:06 +0900

    MemberVO 추가

--- src/domain/MemberVO.java
+++ src/domain/MemberVO.java
@@ -1,5 +1,20 @@
 package domain;
 
-public class MemberVO {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class MemberVO {
+	private String id;
+	private String password;
+	private String name;
+	private String addr;
 }
